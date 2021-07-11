7c1f9f0e75d59fb105e20fe5c7a5c9be7261150a
SHIVAM MISHRA <49128498+shivmishra572@users.noreply.github.com>
2021-07-12 01:03:13 +0530

    Create Plus_One.java

---
+++ Arrays/Plus_One.java
@@ -0,0 +1,59 @@
+// Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+// The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+// You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+ 
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Example 3:
+
+// Input: digits = [0]
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+  
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int[] ans  = new int[digits.length+1];
+        int i=digits.length-1;
+        int carry = 0;
+        while(i>=0){
+            if(i == digits.length-1){
+                int sum = digits[i]+1;
+                carry = sum/10;
+                ans[i+1] = sum%10;
+                i--;
+            }
+            else{
+                int sum = digits[i]+carry;
+                carry = sum/10;
+                ans[i+1] = sum%10;
+                i--;
+            }
+        }
+        ans[0] = carry;
+        if(carry == 0){
+            int[] ans1 = new int[digits.length];
+            for(int j=1;j<ans.length;j++){
+                ans1[j-1] = ans[j];
+            }
+            return ans1;
+        }
+        return ans;
+    }
+}
