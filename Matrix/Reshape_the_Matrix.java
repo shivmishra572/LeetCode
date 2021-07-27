bc350846879b50a77d61db491847113eea7034fe
Shivam Mishra <49128498+shivmishra572@users.noreply.github.com>
2021-07-28 00:59:28 +0530

    Update Reshape_the_Matrix.java

--- Matrix/Reshape_the_Matrix.java
+++ Matrix/Reshape_the_Matrix.java
@@ -1,3 +1,5 @@
+//Approach 1
+
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int row = mat.length;
@@ -18,6 +20,31 @@ class Solution {
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 ans[i][j] = arr[index++];
+            }
+        }
+        
+        return ans;
+    }
+}
+
+//Approach 2
+
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int row = mat.length;
+        int cols = mat[0].length;
+        
+        if(row*cols != r*c) return mat;
+        
+        int[][] ans = new int[r][c];
+        int ri=0,cj=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<cols;j++){
+                ans[ri][cj++] = mat[i][j];
+                if(cj>=ans[0].length){
+                    cj = 0;
+                    ri++;
+                }
             }
         }
         
