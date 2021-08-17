d7cf6f096eb8b45b699cbb714b25d9ed430fbe6b
Shivam Mishra <49128498+shivmishra572@users.noreply.github.com>
2021-08-17 15:46:05 +0530

    Searching
    Binary Search

--- Arrays/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Arrays/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,5 +1,7 @@
+Problem Link : https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 Time Complexity = O(logn)
 
+// # Approach : 1
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = {-1,-1};
@@ -35,5 +37,58 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+
+// # Approach : 2
+    
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        int[] arr = {-1,-1};
+        
+        //If length of array is 0
+        if(nums.length == 0) return arr;
+        //Binary Search for Searching First position
+        
+        int start = 0;
+        int end = nums.length-1;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid] == target){
+                arr[0] = mid;
+                end = mid-1;
+            }
+            else if(target < nums[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        
+        //Binary Search for Searching Last Position
+        
+        start = 0;
+        end = nums.length-1;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid] == target){
+                arr[1] = mid;
+                start = mid+1;
+            }
+            else if(target < nums[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return arr;
     }
 }
