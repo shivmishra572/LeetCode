49868224f4badef1118642643a3ddeffadb594d0
Shivam Mishra <49128498+shivmishra572@users.noreply.github.com>
2021-08-17 14:43:38 +0530

    Update Find_Smallest_Letter_Greater_Than_Target.java

--- Strings/Find_Smallest_Letter_Greater_Than_Target.java
+++ Strings/Find_Smallest_Letter_Greater_Than_Target.java
@@ -29,3 +29,23 @@ class Solution {
         return letters[start];
     }
 }
+
+//Approach 2
+
+ public char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
