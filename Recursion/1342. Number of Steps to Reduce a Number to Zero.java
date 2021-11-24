e8e1caab0cb411def10cf0ab6a3fbece522baf8b
Shivam Mishra <49128498+shivmishra572@users.noreply.github.com>
2021-11-25 00:43:02 +0530

    Recursion

---
+++ Recursion/1342. Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int ans = count(num,0);
+        return ans;
+    }
+    
+    public int count(int num,int step){
+        
+        if(num == 0){
+            return step;
+        }
+        
+        if(num%2 == 0){
+            return count(num/2,step+1);
+        }
+        
+        return count(num-1,step+1);
+    }
+}
